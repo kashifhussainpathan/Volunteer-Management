56f93a4abef60116f78a85ff9f542fdd6b6dcf24
KashifPathan <kashifpathan.kp.kp@gmail.com>
2023-10-31 21:19:04 +0530

    added github link

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -10,6 +10,11 @@ const Navbar = () => {
       <div className="flex navbar_utils">
         <NavLink to="/events"> Events</NavLink>
         <NavLink to="/volunteers"> Volunteers</NavLink>
+        <a
+          href="https://github.com/kashifhussainpathan/Volunteer-Management"
+          target="_blank"
+          rel="noopener noreferrer"
+        ></a>
       </div>
     </div>
   );
